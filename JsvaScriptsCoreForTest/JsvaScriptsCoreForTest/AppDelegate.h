cc18f0928a1c0aacbf4e59ec08b0db118129f63d
张泽 <zhangzeStrong@126.com>
2016-09-01 14:26:48 +0800

    提交项目
    提交项目

---
+++ JsvaScriptsCoreForTest/JsvaScriptsCoreForTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsvaScriptsCoreForTest
+//
+//  Created by yaoln on 16/9/1.
+//  Copyright © 2016年 zhangze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
